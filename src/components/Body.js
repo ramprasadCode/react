5d35f2e67880b43b6feb9fcbcde2e48dedd292b3
Ram Prasad Nomula <ramprasad.vce@gmail.com>
2023-08-08 11:13:50 +0530

    updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { resList } from "../utils/staticData";
 import RestarentCard from "./RestarentCard";
 
 const Body = () => {
+    const [listOfRetarents,setListOfRetos] = useState(resList);
     return <div className = "body">
-        <div className = "search">search</div>
+        <div className = "filter">
+            <button className="filter-btn" 
+            onClick={()=> {
+                const filterdList = listOfRetarents.filter(
+                    (res) => res.info.avgRating > 4 
+                    );
+                    setListOfRetos(filterdList);
+            }}> Filter by 4 rating
+                
+                </button></div>
+
         <div className = "res-container">
         {
-            resList.map(restaurent => <RestarentCard key = {restaurent.info.id} resData = {restaurent}/>)
+            listOfRetarents.map(restaurent => <RestarentCard key = {restaurent.info.id} resData = {restaurent}/>)
         };
         </div>
     </div>
 }
 
-export default Body;
+export default Body; 
